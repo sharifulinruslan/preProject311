3d6edd310462242c178126318537f2bc42604c67
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-03-26 15:35:05 +0400

    Init Spring Boot

---
+++ src/main/java/com/sharrusr/preProject311/PreProject311Application.java
@@ -0,0 +1,13 @@
+package com.sharrusr.preProject311;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreProject311Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreProject311Application.class, args);
+	}
+
+}
