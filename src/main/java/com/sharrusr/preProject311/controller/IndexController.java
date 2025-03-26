05fbed3b0d93d48d3eae7b05baed8bc304e9924f
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-03-26 16:08:26 +0400

    Qualified which UserService will be used

--- src/main/java/com/sharrusr/preProject311/controller/IndexController.java
+++ src/main/java/com/sharrusr/preProject311/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.sharrusr.preProject311.controller;
 import com.sharrusr.preProject311.model.User;
 import com.sharrusr.preProject311.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 @RequestMapping("/users")
 public class IndexController {
+    @Qualifier("userServiceImplWithRepository")
     @Autowired
     private UserService userService;
 
